df4bc22c950366af1b28880fa213619a5a7207c3
m1k4x00 <142576207+m1k4x00@users.noreply.github.com>
2023-08-24 13:42:05 +0300

    Add files via upload

---
+++ CreateUsers.ps1
@@ -0,0 +1,22 @@
+﻿
+$names = Get-Content .\names.txt
+$defaultPass = "Password1"
+
+$password = ConvertTo-SecureString $defaultPass -AsPlainText -Force
+New-ADOrganizationalUnit -Name USERS -ProtectedFromAccidentalDeletion $false
+
+foreach ($i in $names) {
+    $fistName = $i.split(" ")[0].ToLower()
+    $lastName = $i.split(" ")[1].ToLower()
+    $username = "$($fistName.substring(0,1))$($lastName)".ToLower()
+    Write-Host "Creating user: $($username)"
+
+    New-AdUser -AccountPassword $password `
+               -GivenName $fistName `
+               -Surname $lastName `
+               -DisplayName $username `
+               -Name $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=USERS,$(([ADSI]`"").distinguishedName)"`
+               -Eanabled $true
+}
